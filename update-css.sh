a13cb234bfa92b3f10fe4255cfc23d073c24e083
Adrian Winterstein <adrian@winterstein.biz>
2025-02-28 20:53:32 +0100

    Updated TailwindCSS to version 4.0

--- update-css.sh
+++ update-css.sh
@@ -7,7 +7,7 @@ SOURCE_DIR=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
 docker build -t develop-zola-tailwindcss "${SOURCE_DIR}" || exit 2
 
 # update the CSS file using tailwindcss and the JavaScript files using uglifyjs
-docker run -v ${SOURCE_DIR}:/source:Z -w /source develop-zola-tailwindcss \
+docker run --rm -v ${SOURCE_DIR}:/source:Z -w /source develop-zola-tailwindcss \
     sh -c "uglifyjs ./src/js/main.js --compress --mangle -o ./static/js/main.js &&
            uglifyjs ./src/js/page.js --compress --mangle -o ./static/js/page.js &&
            uglifyjs ./src/js/search.js --compress --mangle -o ./static/js/search.js &&
